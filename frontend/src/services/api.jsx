0b596ce4ac375f84395cdbe9b205ccf57a42fc8d
vaishnavi2628 <vaishnavimishra840@gmail.com>
2026-07-09 22:10:28 +0530

    backend url

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "https://legalcms-backend.azurewebsites.net/api",
+  baseURL: "https://legalcms-backend.azurewebsites.net/api",
 });
 
 API.interceptors.request.use((config) => {
